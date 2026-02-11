193f6bfda2ddac926a146e661f62447947706e41
ahmedkom <90234347+ahmedkom@users.noreply.github.com>
2026-02-11 13:43:57 +0200

    Replace iframe with complete HTML structure

--- on drama.js
+++ on drama.js
@@ -1,1 +1,16 @@
-<iframe width="560" height="315" src="https://www.elahmad.com/tv/mobiletv/glarb.php?id=on_drama" style="border:none;" allowfullscreen allow="autoplay; fullscreen; picture-in-picture; xr-spatial-tracking; encrypted-media"></iframe>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body, html { margin: 0; padding: 0; height: 100%; overflow: hidden; background: black; }
+        iframe { width: 100%; height: 100%; border: none; }
+    </style>
+</head>
+<body>
+    <iframe src="https://www.elahmad.com/tv/mobiletv/glarb.php?id=on_drama" 
+            allowfullscreen 
+            allow="autoplay; fullscreen; picture-in-picture">
+    </iframe>
+</body>
+</html>
