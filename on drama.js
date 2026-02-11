f5a111ffbb4f0e5415e96fc3cb46e1d828473eac
ahmedkom <90234347+ahmedkom@users.noreply.github.com>
2026-02-11 13:06:36 +0200

    Create on drama.js

---
+++ on drama.js
@@ -0,0 +1,1 @@
+<iframe width="560" height="315" src="https://www.elahmad.com/tv/mobiletv/glarb.php?id=on_drama" style="border:none;" allowfullscreen allow="autoplay; fullscreen; picture-in-picture; xr-spatial-tracking; encrypted-media"></iframe>
