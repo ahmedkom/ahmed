5f94b62bc0b34e018d458f5767ca3743052b1769
ahmedkom <90234347+ahmedkom@users.noreply.github.com>
2026-02-11 23:24:42 +0200

    Delete on drama.js

--- on drama.js
+++
@@ -1,9 +0,0 @@
-{
-  "channels": [
-    {
-      "name": "اون دراما",
-      "url": "<iframe width=\"560\" height=\"315\" src=\"https://www.elahmad.com/tv/mobiletv/glarb.php?id=on_drama\" style=\"border:none;\" allowfullscreen allow=\"autoplay; fullscreen; picture-in-picture; xr-spatial-tracking; encrypted-media\"></iframe>",
-      "type": "iframe": true
-    }
-  ]
-}
