8cdb2decf2331455e8cd27e1d56f958895691489
ahmedkom <90234347+ahmedkom@users.noreply.github.com>
2026-02-11 13:50:27 +0200

    Fix JSON syntax for 'type' field in on drama.js

--- on drama.js
+++ on drama.js
@@ -3,7 +3,7 @@
     {
       "name": "اون دراما",
       "url": "<iframe width=\"560\" height=\"315\" src=\"https://www.elahmad.com/tv/mobiletv/glarb.php?id=on_drama\" style=\"border:none;\" allowfullscreen allow=\"autoplay; fullscreen; picture-in-picture; xr-spatial-tracking; encrypted-media\"></iframe>",
-      "type": "iframe"
+      "type": "iframe": true
     }
   ]
 }
